0c086837203585c2edbc512ed221b366c3d30261
not-isura <islartg@students.nu-baliwag.edu.ph>
2026-05-05 23:22:04 +0800

    with sql query and updated git ignore

--- Test_Buzzer_and_LED/led-flasher.py
+++ Test_Buzzer_and_LED/led-flasher.py
@@ -3,9 +3,9 @@ from time import sleep
 from led_toggle import ToggleLED
 
 
-toggle_led = ToggleLED(22)
+toggle_led = ToggleLED(23)
 toggle_led.toggle()
-sleep(6)
+sleep(20)
 toggle_led.toggle()
 sleep(1)
 toggle_led.close()
